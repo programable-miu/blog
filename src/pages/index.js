5c3430f30b71107920d5f57050a16ff363f52d18
Miuko Kawasaki <miuko.kawasaki@gmail.com>
2020-08-29 17:10:06 +0900

    日付のフォーマットを日本人にわかりやすいように変更します

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,7 @@ export const pageQuery = graphql`
             slug
           }
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "YYYY年MM月DD日")
             title
             description
           }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -93,7 +93,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "YYYY年MM月DD日")
         description
       }
     }
